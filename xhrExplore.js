4401dbb22b7bf587715dd97ec4cd713d82ed8883
Clint88 <Ccrockett154@west-mec.org>
2020-10-28 13:53:32 -0700

    Explore xhr complete

--- xhrExplore.js
+++ xhrExplore.js
@@ -8,11 +8,12 @@ xhr.addEventListener("readystatechange", function () {
         //console.log(this.responseText);
         const info = JSON.parse(this.responseText);
         console.log(info);
-
+            
         for(var i = 0; i<10; i++){
             var number = info[getRndInteger(1163)];
             var name = info[getRndInteger(1163)];
             var form = info[getRndInteger(1163)];
+            //The vars need to be inside the for loop for each diffrent section so its not all the same pokemon
             document.querySelectorAll(".number")[i].innerHTML = number.pokemon_name;
             document.querySelectorAll(".name")[i].innerHTML = name.pokemon_id;
             document.querySelectorAll(".form")[i].innerHTML = form.form;
@@ -23,7 +24,8 @@ xhr.addEventListener("readystatechange", function () {
 });
 console.log(getRndInteger(1163))
 function getRndInteger(max) {
-    return Math.floor(Math.random() * Math.random(max));
+    return Math.floor(Math.random() * Math.floor(max));
+    //Math.floor goes from the very first number
 }
 
 xhr.open("GET", "https://pokemon-go1.p.rapidapi.com/pokemon_stats.json");
--- xhrInfo.js
+++ xhrInfo.js
@@ -12,10 +12,11 @@ xhr.addEventListener("readystatechange", function () {
         const getBtn = document.getElementById("searchBtn");
         getBtn.onclick = function(){
             const userInput = document.getElementById("search").value;
-
+            //this will put anything the user types and put it into the unserInput
             document.querySelector('.pokeName').innerHTML = info[userInput].pokemon_name;
             document.querySelector('.pokeNum').innerHTML = info[userInput].pokemon_id;
             document.querySelector('.form').innerHTML = info[userInput].form;
+            // once the userInput is found the data will look for what number they typed and find the info on the pokemon
         }
 	}
 });
